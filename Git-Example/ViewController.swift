4900ecf32f32d26f20b6c2e8073149140e453e8c
Alex L <Alex@it.training>
2019-01-05 20:22:34 -0500

    space added

--- Git-Example/ViewController.swift
+++ Git-Example/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let hillsYposition: CGFloat = 500
+        let hillsYposition: CGFloat = 300
+        
     }
 
 
